739aada08d79e33f052fdc0b9b329c84b826b11b
anre18 <42691322+anre18@users.noreply.github.com>
2024-07-19 19:41:27 +0200

    Fix Raspberry Service

--- RaspberryService/setup.sh
+++ RaspberryService/setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 sudo apt install python3-pip;
-sudo apt remove python3-rpi.gpio;
+sudo apt install python3-pil;
+sudo apt install python3-numpy;
+sudo apt-get install python3-serial
 pip3 install rpi-lgpio --break-system-packages;
 pip3 install evdev --break-system-packages;
-sudo apt install python3-pil;
-sudo apt-get install python3-numpy;
 pip3 install spidev --break-system-packages;
-sudo apt-get remove --purge python3-serial;
-sudo apt-get install python3-serial;
